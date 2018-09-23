3a19ed946d974aac623fd1e4809762fabe460250
Ting007 <twa700@yahoo.com>
2018-09-22 23:06:05 -0400

    have the program in OOD

--- part1/phoneName.py
+++ part1/phoneName.py
@@ -2,19 +2,39 @@ import sys
 import re
 import collections
 
-def stdPhone(num):
-	"""
-	input: str, num is the phone number
-	output: str, num that is standardized as (999)000-1234
-	"""
-	if num == "":
-		return ''
-	number = re.split("[()-.]", num)
-	number = ''.join(number)
-	if len(number) == 10:
-		return '('+number[:3]+')'+number[3:6]+'-' + number[6:]
-	else:
-		return number[:3]+'-'+number[3:]
+class PhonePerson():
+	"""docstring for PhoneNumber"""
+	numPerson = collections.defaultdict(list) # a dictionary to hold the phone number and the name
+
+	def __init__(self, number, name):
+		"""
+		constructor
+		"""
+		self.num = number
+		self.name = name
+		self.stdNum = None
+
+	def stdPhone(self):
+		"""
+		self.num is the phone number from the input file
+		num may and may not be standard
+		set the self.stdNum as standardized as (999)000-1234
+		if not with area code standardized as 123-2345
+		"""
+		if self.num == "":
+			self.stdNum = ''
+		number = re.split("[()-.]", self.num)
+		number = ''.join(number)
+		if len(number) == 10:
+			self.stdNum = '('+number[:3]+')'+number[3:6]+'-' + number[6:]
+		else:
+			self.stdNum = number[:3]+'-'+number[3:]
+
+	def setnumPerson(self):
+		PhonePerson.numPerson[self.stdNum].append(self.name)
+
+	def getNumPerson():
+		return PhonePerson.numPerson
 
 
 if __name__ == '__main__':
@@ -23,15 +43,22 @@ if __name__ == '__main__':
 	dis = f.readlines();
 	f.close
 
-	numPerson = collections.defaultdict(list) # a dictionary to hold the phone number and the name
+	res = []
 
 	for i in dis:
 		content = re.split("[\t\n]", i)
-		phoneNo = stdPhone(content[1])
-		numPerson[phoneNo].append(content[0])
+		phone = content[1]
+		name = content[0]
+		res.append(PhonePerson(phone, name))
+
+	for item in res:
+		item.stdPhone()
+		item.setnumPerson()
+
+	Phonelist = PhonePerson.getNumPerson()
 
 	with open(sys.argv[2], 'w') as file:
-		for phone, name in numPerson.items():
+		for phone, name in Phonelist.items():
 			file.write(phone+'\t'+','.join(name)+'\n')
 
 
--- part2/mutate.py
+++ part2/mutate.py
@@ -1,7 +1,11 @@
 import sys
 
-def changeProtein(proteinFile, position, mutation):
-	codon = {'ATT':'I', 'ATC':'I', 'ATA':'I', 'CTT':'L', 'CTC':'L', 'CTA':'L',\
+class Protein():
+	"""docstring for Protein
+	codonlist is the harsh map {codon: amino acid}
+	"""
+
+	codonlist = {'ATT':'I', 'ATC':'I', 'ATA':'I', 'CTT':'L', 'CTC':'L', 'CTA':'L',\
 	'CTG':'L', 'TTA':'L', 'TTG':'L', 'GTT':'V', 'GTC':'V', 'GTA':'V', 'GTG':'V',\
 	'TTT':'F', 'TTC':'F', 'ATG':'M', 'TGT':'C', 'TGC':'C', 'GCT':'A', 'GCC':'A',\
 	'GCA':'A', 'GCG':'A', 'GGT':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G', 'CCT':'P',\
@@ -11,47 +15,97 @@ def changeProtein(proteinFile, position, mutation):
 	'CAC':'H', 'GAA':'E', 'GAG':'E', 'GAT':'D', 'GAC':'D', 'AAA':'K', 'AAG':'K',\
 	'CGT':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R', 'AGA':'R', 'AGG':'R'}
 
-	f = open(proteinFile, 'r')
-	next(f)
-	protein_seq = f.readlines()
-	f.close
-	protein = ''
-	for frag in protein_seq:
-		protein += frag.rstrip()
-	res = list(protein)
-	res[position] = codon[mutation]
-	return ''.join(res)
+	def __init__(self):
+		#geneSeq is the gene sequence
+		#proteinSeq is the protein sequence
+		#mutation is the codon as a result of mutation
+		self.geneSeq = None
+		self.proteinSeq = None
+		self.mutation = None
+
+	def setGeneSeq(self, geneFile):
+		"""
+		setter for geneSeq
+		setGeneSeq(geneFile): read the DNA-seq file and set self.geneseq
+		geneFile: str, filename of gene sequence
+		self.geneSeq: str
+		"""
+		f = open(geneFile, 'r')
+		next(f)
+		dis = f.readlines()
+		f.close
+		gene = ''
+		for fragment in dis:
+			gene += fragment.rstrip()
+		self.geneSeq = gene
+
+	def setProtSeq(self, protFile):
+		"""
+		setter for proteinSeq
+		setProtSeq(protFile): read the protein filr and set set.proteinSeq
+		protFile: str, filename of protein sequence
+		self.proteinSeq: str
+
+		"""
+		f = open(protFile, 'r')
+		next(f)
+		protein_seq = f.readlines()
+		f.close
+		protein = ''
+		for frag in protein_seq:
+			protein += frag.rstrip()
+		self.proteinSeq = protein
+
+	def geneMutate(self, g_pos, gene_loc, mutate):
+		"""
+		setter for self.mutation
+		g_pos: int, is the location of mutation counting from the start codon atg of CDS
+		gene_location: int, is the location of the base of the whole gene including the fragment before CDS
+		mutate: char, is the designed mutation
+		self.mutation: str, the codon after mutation
+		"""
+		if g_pos%3 == 0:
+			codon = self.geneSeq[gene_loc-2]+self.geneSeq[gene_loc-1]+mutate
+		if g_pos%3 == 1:
+			codon = mutate +self.geneSeq[(gene_loc+1)] +self.geneSeq[(gene_loc +2)]
+		if g_pos%3 == 2:
+			codon = self.geneSeq[(gene_loc-1)] + mutate + self.geneSeq[(gene_loc + 1)]
+		self.mutation = codon
+
+
+
+	def mutateProtein(self, position):
+		"""
+		mutate the amino acid of protein at postion into the amimo acid /
+		determined by the codon of self.mutation.
+		position: int, is the position of amino acid in the protein sequence
+		rtype: str, the sequence of mutated protein
+
+		"""
+		res = list(self.proteinSeq)
+		res[position] = Protein.codonlist[self.mutation]
+		return ''.join(res)
 
 if __name__ == '__main__':
-	f = open(sys.argv[1], 'r')
-	next(f)
-	# sys.argv[1] must be the file name of the input file.
-	dis = f.readlines()
-	f.close
+	ADA = Protein()
+	ADA.setGeneSeq(sys.argv[1])
+	ADA.setProtSeq(sys.argv[2])
 
-	#define the position of gene
-	g_pos = int(sys.argv[2])
+	#define the position of gene where the mutation occur
+	#g_pos is the postion counting from start codon
+	g_pos = int(sys.argv[3])	
+	#gene_loc is the postion of mutation from the start of gene
+	gene_loc = g_pos+int(sys.argv[4])-2
+	#find out the mutated codon
+	ADA.geneMutate(g_pos, gene_loc, sys.argv[5])
 
-	gene = ''
-	for fragment in dis:
-		gene += fragment.rstrip()
-
-	gene_loc = g_pos+int(sys.argv[3])-2
-
-	codon = ''
-	if g_pos%3 == 0:
-		codon = gene[gene_loc-2]+gene[gene_loc-1]+sys.argv[4]
-	if g_pos%3 == 1:
-		codon = sys.argv[4] + gene[(gene_loc+1)] + gene[(gene_loc +2)]
-	if g_pos%3 == 2:
-		codon = gene[(gene_loc-1)] + sys.argv[4] + gene[(gene_loc + 1)]
-	print(codon)
-	#find the position of amino acid
+	#find the position of mutated amino acid
 	a_pos = g_pos//3
 	if g_pos%3==0:
 		a_pos -= 1
-	proteinFile = sys.argv[5]
-	res = changeProtein(proteinFile, a_pos, codon)
+
+	#mutate the protein
+	res = ADA.mutateProtein(a_pos)
 	print(res)
 
 
